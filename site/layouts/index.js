b96f37b5910e7df0f016b124d30be60754dfa631
RvdV <cto@yahmarketing.com.au>
2023-11-10 01:44:55 +0000

    Bork Bork Bork Bork

--- site/layouts/index.js
+++
@@ -1,7 +0,0 @@
-// JS Goes here - ES6 supported
-
-import "./css/main.scss";
-
-// Say hello
-// eslint-disable-next-line no-console
-console.log("🦊 Moshi Moshi Index Desu!");
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // JS Goes here - ES6 supported
 
-// import "./css/main.scss";
+import "./css/main.scss";
 
 // Say hello
 // eslint-disable-next-line no-console
